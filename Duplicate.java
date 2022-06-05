b4dd2e24f1779252b690d3d152d2c5399f821654
AnupamTekale <65555514+AnupamTekale@users.noreply.github.com>
2022-06-05 22:18:48 +0530

    Update Duplicate.java

--- Duplicate.java
+++ Duplicate.java
@@ -1,27 +1,50 @@
 import java.util.*;
-class Duplicate
-{
-    public static void main(String args[])
-    {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++)
-            arr[i]=in.nextInt();
+// class Duplicate
+// {
+//     public static void main(String args[])
+//     {
+//         Scanner in = new Scanner(System.in);
+//         int n = in.nextInt();
+//         int arr[] = new int[n];
+//         for(int i=0;i<n;i++)
+//             arr[i]=in.nextInt();
         
-        for(int i=0;i<n;i++)
-        {
-            int ele = Math.abs(arr[i]);
-            if(arr[ele]<0)
-                return ele;
+//         for(int i=0;i<n;i++)
+//         {
+//             int ele = Math.abs(arr[i]);
+//             if(arr[ele]<0)
+//                 return ele;
             
-            arr[ele] = -arr[ele];
+//             arr[ele] = -arr[ele];
+//         }
+        
+//         return n;
+            
+        
+//     }
+// }
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+    // slow and fast pointer approach
+        int slow = nums[0],fast = nums[0];
+        
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        while(slow!=fast);
+        
+        fast = slow[0];
+        
+        while(slow!=fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
         }
         
-        return n;
-            
-        
     }
+    
 }
-        
   
